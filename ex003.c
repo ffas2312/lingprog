5b58f725636e33357a8b65e63802a0d4e79812e6
ffas2312 <fernando.sousa2312@gmail.com>
2016-03-10 22:21:52 -0300

    exercicio aula

---
+++ ex003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+		printf("O maior numero digitado foi "\n);
+		if(x > y){
+			printf("%d\n", x);
+					} else {
+						printf("%d\n", y);
+					}
+					return 0;
+
+} 
